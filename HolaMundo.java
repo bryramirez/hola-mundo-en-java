635533355b3e175acae17656779dde8077d6a591
Bryan <bryramir@gmail.com>
2017-01-11 17:41:56 -0300

    saludos en colorcitus

--- HolaMundo.java
+++ HolaMundo.java
@@ -4,7 +4,7 @@ public static void main(String[] args){
 String naranja = "\033[33m";
 String azul = "\033[34m";
 
-	System.out.println("hola mundo");
-	System.out.println("aqui estamos haciendo pruebas con git");
+	System.out.println(naranja + "hola mundo");
+	System.out.println(azul + "aqui estamos haciendo pruebas con git");
   	}
 }
